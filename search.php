5c1016bead2548f1c2a6751332aa13c1328bc057
Lorenzo <lorenzocorrea.ge@gmail.com>
2021-01-26 17:51:42 -0300

    Colocando quantidade de registros

--- search.php
+++ search.php
@@ -113,7 +113,7 @@ if($page){
                     <form action="search.php">
                         <div class="input-group mb-3">
                             <input type="text" class="form-control" name="arquivo" placeholder="Busque um arquivo..." aria-label="Busque um arquivo...">
-                            <span id="qtd-reg-filter" style="position: absolute; right: 0; top: 40px;">7 registros encontrados</span>
+                            <span id="qtd-reg-filter" style="position: absolute; right: 0; top: 40px;">Quantidade de registro<?=count($dowloadsArchives) > 1 ? "s" : "";?> encontrado <?=count($dowloadsArchives);?></span>
                             <button type="submit" class="input-group-text button-filter" id="btnSearch"><i class="fas fa-search"></i></button>
                         </div>
                     </form>
