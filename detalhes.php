18c3422fe39d1f5ce11e476d2deedab68ac2070d
Camila Moreira <camilamoreira.webdesigner@hotmail.com>
2021-02-09 09:00:41 -0300

    pagina de detalhes do curso em construção

---
+++ detalhes.php
@@ -0,0 +1,122 @@
+<?php
+require 'assets/includes/functions.php';
+$page = filter_input(INPUT_GET, 'page');
+$value = filter_input(INPUT_GET, 'value');
+$categoria = filter_input(INPUT_GET, 'cat');
+$courses = [];
+if ($value) {
+    $courses = searchCourses($value, $page, $categoria);
+} else {
+    $courses = getCourses($page, $categoria);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <title>Ectos - Educação Executiva</title>
+
+    <!-- Links do site -->
+    <link href="assets/css/cursos.css" rel="stylesheet">
+
+    <!-- Links do Bootstrap 5 -->
+    <link href="assets/libraries/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="assets/libraries/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Links do Fontswesome 5.15.1 -->
+    <link href="assets/libraries/fontawesome/css/all.css" rel="stylesheet">
+
+    <!-- Link do Google Fonts - Rubik Light 300, Regular 400, Medium 500 e Bold 700 -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+    <!-- Link do Jquery 1.10.2 -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+    <style>
+        .navbar-default {
+            position: inherit !important;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- TOP MENU -->
+    <div id="top-menu">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    <span>
+                        <a href="mailto:contato@ectos.com.br">
+                            <i class="far fa-envelope"></i> contato@ectos.com.br
+                        </a>
+                    </span>
+                    <span>
+                        <a href="https://www.instagram.com/ectosoficial/" target="blank">
+                            <i class="fab fa-instagram"></i> ectosoficial
+                        </a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- MENU -->
+    <nav id="nabvar-default" class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="assets/img/logomarca.png" alt="Ectos" height="30px">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">INICIO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#quem-somos">QUEM SOMOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#servicos">SERVIÇOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="cursos.php">CURSOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="downloads.php">DOWNLOADS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contato">CONTATO</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        <div id="rodape" class="row">
+            <div class="col-xs-12 col-sm-12 col-md-6 text-start">
+                &copy; Ectos, 2020. Todos os direitos reservados.
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-6 text-end">
+                <span><a href="http://squadweb.com.br/" target="blank">Desenvolvido por Squad Web</a>.</span>
+            </div>
+        </div>
+    </div>
+
+    <div id="goTop" class="back-to-top">
+        <a href="#"><i class="fas fa-arrow-circle-up fa-3x"></i></a>
+    </div>
+
+    <!-- Link do site -->
+    <script src="assets/js/botaoTopo.js"></script>
+
+</body>
+
+</html>
