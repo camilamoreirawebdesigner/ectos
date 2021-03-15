0a50726b51e116086a6b7deb5ef2e32b8144f40c
Lorenzo <lorenzocorrea.ge@gmail.com>
2021-03-15 15:53:02 -0300

    arrumando emails

--- assets/email.php
+++ assets/email.php
@@ -59,19 +59,28 @@ use PHPMailer\PHPMailer\Exception;
 // Load Composer's autoloader
 require '../vendor/autoload.php';
 
+//PUXANDO CONEXÃO COM O BANCO 
+require '../assets/includes/conection.php';
+
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
+
+    $sql = $pdo->query("SELECT * FROM parameters");
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+    
+    
+
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'mail.camilamoreira.com.br';                    // Set the SMTP server to send through
+    $mail->Host       = $lista[0]['servidor'];                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'contato@camilamoreira.com.br';                     // SMTP username
-    $mail->Password   = 'm4nz0l1@';                               // SMTP password
+    $mail->Username   = $lista[0]['usuario'];                     // SMTP username
+    $mail->Password   = $lista[0]['senha'];                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = $lista[0]['porta'];                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom('contato@camilamoreira.com.br', 'Camila');
